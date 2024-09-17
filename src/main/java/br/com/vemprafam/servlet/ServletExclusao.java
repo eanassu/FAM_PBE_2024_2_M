fbb0c1b60b140db3b9833763512c3479b6c6870b
eanas <eanas@DESKTOP-LNCRKBU>
2024-09-17 20:50:39 -0300

    Aula 17/9

--- src/main/java/br/com/vemprafam/servlet/ServletExclusao.java
+++ src/main/java/br/com/vemprafam/servlet/ServletExclusao.java
@@ -2,9 +2,6 @@ package br.com.vemprafam.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.NumberFormat;
-import java.text.SimpleDateFormat;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,16 +13,16 @@ import br.com.vemprafam.dao.DaoAluno;
 import br.com.vemprafam.pojo.Aluno;
 
 /**
- * Servlet implementation class ServletTabela
+ * Servlet implementation class ServletExclusao
  */
-@WebServlet("/Tabela")
-public class ServletTabela extends HttpServlet {
+@WebServlet("/Exclusao")
+public class ServletExclusao extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ServletTabela() {
+    public ServletExclusao() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -34,30 +31,23 @@ public class ServletTabela extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
+		int ra = Integer.parseInt(request.getParameter("ra"));
 		DaoAluno dao = new DaoAluno();
-		List<Aluno> lista = dao.getLista();
+		Aluno a = new Aluno(ra, null, null, null, null, null);
+		dao.excluir(a);
+		PrintWriter out = response.getWriter();
 		out.println("<!DOCTYPE html>\r\n"
 				+ "<html>\r\n"
 				+ "<head>\r\n"
 				+ "<meta charset=\"ISO-8859-1\">\r\n"
-				+ "<title>Tabela</title>\r\n"
+				+ "<title>Resultado</title>\r\n"
 				+ "</head>\r\n"
 				+ "<body>\r\n"
-				+ "<table border='1'>\r\n"
-				+ "<tr>\r\n"
-				+ "	<th>RA</th><th>nome</th><th>data nasc.</th><th>curso</th><th>renda</th><th>e-mail</th>\r\n"
-				+ "</tr>");
-		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
-		for( Aluno a:lista ) {
-			out.println("<tr>\r\n"
-					+ "	<td>"+a.getRa()+"</td><td>"+a.getNome()+"</td><td>"+ dateFormat.format( a.getDataNascimento())+"</td>"
-					+ "<td>"+a.getCurso()+"</td><td>"+ numberFormat.format( a.getRenda())+"</td><td>"+a.getEmail()+"</td>\r\n"
-					+ "</tr>");
-		}
-		out.println("</table>"
-				+ "<a href='/Projeto'>voltar</a>\r\n"
+				+ "<h1>Exclusão</h1>\r\n"
+				+ "\r\n"
+				+ "<p>Aluno excluído</p>\r\n"
+				+ "<a href='/Projeto'>voltar</a>"
+				+ "\r\n"
 				+ "</body>\r\n"
 				+ "</html>");
 	}
